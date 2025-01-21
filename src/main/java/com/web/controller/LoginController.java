5a214f7dedbbd880bbc1c32b9b08b9f8cc889f76
dongzhen <1505687245@qq.com>
2025-01-21 09:15:26 +0800

    demo

---
+++ src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author dongzhen
+ * @CreateTime: 2025-01-21  09:14
+ **/
+@RestController
+@RequestMapping("/users/login")
+public class LoginController {
+}
